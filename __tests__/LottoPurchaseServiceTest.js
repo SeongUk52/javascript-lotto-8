fac80bc077322a2f110b945376787cb282930541
SeongUk52 <rkfka7849@gmail.com>
2025-11-03 08:50:54 +0900

    feat: 로또 구매 서비스 - 중복 없는 6개 번호 생성 기능 추가

--- __tests__/LottoPurchaseServiceTest.js
+++ __tests__/LottoPurchaseServiceTest.js
@@ -1,5 +1,6 @@
 import LottoPurchaseService from "../src/service/LottoPurchaseService";
 import PurchaseAmount from "../src/PurchaseAmount";
+import { MissionUtils } from "@woowacourse/mission-utils";
 
 describe("로또 구매 서비스 테스트", () => {
   test("구매 금액에 따른 로또 개수를 계산한다.", () => {
@@ -9,6 +10,19 @@ describe("로또 구매 서비스 테스트", () => {
     const lottoCount = service.calculateLottoCount(purchaseAmount);
     
     expect(lottoCount).toBe(14);
+  });
+
+  test("중복 없는 6개 번호를 생성한다.", () => {
+    const service = new LottoPurchaseService();
+    
+    const numbers = service.generateNumbers();
+    
+    expect(numbers).toHaveLength(6);
+    expect(new Set(numbers).size).toBe(6);
+    numbers.forEach((number) => {
+      expect(number).toBeGreaterThanOrEqual(1);
+      expect(number).toBeLessThanOrEqual(45);
+    });
   });
 });
 
--- src/service/LottoPurchaseService.js
+++ src/service/LottoPurchaseService.js
@@ -1,6 +1,12 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class LottoPurchaseService {
   calculateLottoCount(purchaseAmount) {
     return purchaseAmount.getLottoCount();
+  }
+
+  generateNumbers() {
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
   }
 }
 

58c11763deb8e545708fddb74a2f6a8daa48e0b7
SeongUk52 <rkfka7849@gmail.com>
2025-11-03 09:25:33 +0900

    feat: 당첨 번호 및 보너스 번호 입력 기능 추가
    - InputView: 당첨 번호 입력 기능 (프롬프트 출력, 입력 받기 분리)
    - InputView: 보너스 번호 입력 기능 (프롬프트 출력, 입력 받기 분리)
    - 한 메서드가 한 가지 기능만 담당하도록 분리
    - README 기능 목록 업데이트

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -13,6 +13,32 @@ class InputView {
   static async readPurchaseAmountInput() {
     return await MissionUtils.Console.readLineAsync();
   }
+
+  static async readWinningNumbers() {
+    InputView.printWinningNumbersPrompt();
+    return await InputView.readWinningNumbersInput();
+  }
+
+  static printWinningNumbersPrompt() {
+    MissionUtils.Console.print("당첨 번호를 입력해 주세요.");
+  }
+
+  static async readWinningNumbersInput() {
+    return await MissionUtils.Console.readLineAsync();
+  }
+
+  static async readBonusNumber() {
+    InputView.printBonusNumberPrompt();
+    return await InputView.readBonusNumberInput();
+  }
+
+  static printBonusNumberPrompt() {
+    MissionUtils.Console.print("보너스 번호를 입력해 주세요.");
+  }
+
+  static async readBonusNumberInput() {
+    return await MissionUtils.Console.readLineAsync();
+  }
 }
 
 export default InputView;
